013cc6d866481bd8342a5be266fef8b07decf44f
ATIYYATULLAH-GIRL <mabas2014.am@gmail.com>
2025-12-02 09:36:30 -0800

    Add files via upload

---
+++ password.py
@@ -0,0 +1,5 @@
+import random, string
+chars=string.ascii_letters + string.digits
+length = 12
+password = ''.join(random.choice(chars) for _ in range(length))
+print("Generated Password:", password)
